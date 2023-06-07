7c701964558d93854321b9441e54f9e8ff79b70e
denis <denis.omondi@student.moringaschool.com>
2023-06-07 16:57:32 +0300

    Finished users model, controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,35 @@
+# controllers/users_controller.rb
 class UsersController < Sinatra::Base
-    # ...
+    # GET /users
+    get '/users' do
+      users = User.all
+      users.to_json(include: :orders)
+    end
   
+    # GET /users/:id
     get '/users/:id' do
       user = User.find(params[:id])
       user.to_json(include: :orders)
     end
   
-    # ...
+    # POST /users
+    post '/users' do
+      user = User.create(name: params[:name], email: params[:email])
+      user.to_json
+    end
+  
+    # PUT /users/:id
+    put '/users/:id' do
+      user = User.find(params[:id])
+      user.update(name: params[:name], email: params[:email])
+      user.to_json
+    end
+  
+    # DELETE /users/:id
+    delete '/users/:id' do
+      user = User.find(params[:id])
+      user.destroy
+      status 204
+    end
   end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 # models/user.rb
 class User < ActiveRecord::Base
     has_many :orders
+  
+    # Validation example: ensure name and email are present
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+  
+    # You can define instance or class methods as needed
+    def full_address
+      "#{address}, #{city}, #{country}"
+    end
   end
   
